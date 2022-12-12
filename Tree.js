134211867ede6c1842621797e2ca3c237baad323
memmcol-prince <memmcol.prince@gmail.com>
2022-12-12 15:17:15 +0100

    Implemented finding values in tree data structure

--- Tree.js
+++ Tree.js
@@ -67,6 +67,35 @@ class Node {
       }
     }
   }
+
+  find(value) {
+    // console.log(this.value, "~");
+    for (const node of this.childern) {
+      if (node.value === value) {
+        return node;
+      }
+
+      const nestedNode = node.find(value);
+      if (nestedNode) {
+        return nestedNode;
+      }
+    }
+  }
+
+  findUsingBredth(value) {
+    // console.log(this.value, "~");
+    for (const node of this.childern) {
+      if (node.value === value) {
+        return node;
+      }
+    }
+    for (const node of this.childern) {
+      const nestedNode = node.findUsingBredth(value);
+      if (nestedNode) {
+        return nestedNode;
+      }
+    }
+  }
 }
 
 class Tree {
@@ -81,25 +110,19 @@ class Tree {
     this.root.removeChild(path);
   }
 
+  find(value) {
+    if (this.root.value === value) {
+      return this.root;
+    }
+
+    return this.root.findUsingBredth(value);
+  }
   display() {
     return this.root;
   }
 }
 
 const fileSys = new Tree("/");
-// const documents = fileSys.root.addChild("/documents");
-// const movies = fileSys.root.addChild("/movies");
-// const games = fileSys.root.addChild("/games");
-// documents.node.addChild("/corel draw");
-// documents.node.addChild("/microsoft office");
-// documents.node.addChild("/corel photopaint");
-// const marvel = movies.node.addChild("/Marvel studios");
-// const cod = games.node.addChild("/call of duty");
-// marvel.node.addChild("Thor.mp4");
-// marvel.node.addChild("Avengers.mp4");
-// marvel.node.addChild("Thor.mp4");
-// cod.node.addChild("Level1.exe");
-// cod.node.addChild("Level5.exe");
 fileSys.add("documents");
 fileSys.add("documents/corel draw/banner.cdr");
 fileSys.add("documents/corel draw/logo.cdr");
@@ -113,6 +136,10 @@ fileSys.add("movies/Marvel studios/Thor.mp4");
 fileSys.add("games/call of duty/Level1.exe");
 fileSys.add("games/call of duty/Level2.exe");
 // fileSys.remove("games/call of duty/Level2.exe");
-fileSys.remove("games");
+// fileSys.remove("games");
 
 console.log(fileSys.display());
+console.log("Find 'Thor.mp4'", fileSys.find("Thor.mp4"));
+console.log("Find 'movies'", fileSys.find("movies"));
+console.log("Find 'documents'", fileSys.find("documents"));
+console.log("Find 'Marvel studios'", fileSys.find("Marvel studios"));
